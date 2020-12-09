6b3103d5372e39812f34679cb215c15ba12ee554
yuriaFan <1013543433@qq.com>
2020-12-09 17:35:25 +0800

    feat:解决打包路径问题

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  publicPath: "",
-  chainWebpack: config => {
+  publicPath:  process.env.NODE_ENV === 'production'?'/dist/':'/',
+  chainWebpack: (config) => {
     config.module
-      .rule('md')
+      .rule("md")
       .test(/\.md/)
-      .use('html-loader')
-      .loader('html-loader')
+      .use("html-loader")
+      .loader("html-loader")
       .end()
-      .use('markdown-loader')
-      .loader('markdown-loader')
-      .end()
+      .use("markdown-loader")
+      .loader("markdown-loader")
+      .end();
   },
   devServer: {
     port: 8080,
-    open: true
-  }
-}
+    open: true,
+  },
+};
